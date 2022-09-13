614f0161930ae77e80e0ef6921ea1680fbe07b90
Sydney Kuhn <sydney.kuhn@mths.ca>
2022-09-13 14:04:15 +0000

    added hello world

---
+++ src/helloworld.ts
@@ -0,0 +1,11 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * By:      Sydney Kuhn
+ * Version: 1.0
+ * Since:   2022-09-13
+ */
+
+console.log('Hello, World!')
+console.log('\nDone.')
